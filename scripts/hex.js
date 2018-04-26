ac33c6bbbdd9f6318f5a70b2d6deeb4b06805b63
Bridget Keehan <ba.keehan@gmail.com>
2018-04-26 12:25:19 -0400

    mockup notes

--- scripts/hex.js
+++ scripts/hex.js
@@ -1,1 +1,15 @@
 // The hex clock is a hex code that reflects the time of day in #hhmmss format instead of hh:mm:ss format. The background color of the screen is then changed based on the hex code which is created by the current time.
+
+
+// date objects
+// document variables
+
+
+// push text w/n setinterval 1000
+
+// push body-bg w/n setinterval 1000
+
+// actually those probably don't need to be separate functions
+// is it cleaner with two or one?
+
+// one
