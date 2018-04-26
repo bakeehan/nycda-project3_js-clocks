0287c9b59d31f7616bf0eca1ea3f0305e48486b0
Bridget Keehan <ba.keehan@gmail.com>
2018-04-26 14:02:30 -0400

    meant to just do js framework, then i got caught up and wrote all my js

--- scripts/hex.js
+++ scripts/hex.js
@@ -1,15 +1,35 @@
 // The hex clock is a hex code that reflects the time of day in #hhmmss format instead of hh:mm:ss format. The background color of the screen is then changed based on the hex code which is created by the current time.
 
-
-// date objects
 // document variables
+var container = document.getElementById("clockContainer");
 
+function passClock() {
 
-// push text w/n setinterval 1000
+	// date objects
+	var date = new Date();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second= date.getSeconds();
 
-// push body-bg w/n setinterval 1000
+	if (second <10) {
+		var second = "0" + second.toString()
+	}
+	if (minute <10) {
+		var minute = "0" + minute.toString()
+	}
+	if (hour <10) {
+		var hour = "0" + hour.toString()
+	}
 
-// actually those probably don't need to be separate functions
-// is it cleaner with two or one?
+	// console.log(second)
 
-// one
+	container.innerHTML = (hour.toString() + minute.toString() + second.toString());
+
+	let color = (hour.toString() + minute.toString() + second.toString());
+	console.log("#" + color);
+	container.style.backgroundColor = "#" + color;
+}
+
+passClock();
+
+setInterval(passClock, 1000);
